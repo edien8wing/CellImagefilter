d046b9702e1a8759b0de2e610524371a74c34725
edien8wing <35421513+edien8wing@users.noreply.github.com>
2019-05-17 16:55:49 +0800

    Add files via upload

---
+++ arc/ARC.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagefilter.arc;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface ARC {
+    public double process(int num);
+}
---
+++ arc/ARC_gradient.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagefilter.arc;
+
+/**
+ *
+ * @author Administrator
+ */
+
+
+public class ARC_gradient implements ARC{
+    //ps中测试的107 111阀值二值化代码实现
+    int threshold=0;
+     public ARC_gradient(int threshold){
+        this.threshold=threshold;
+     }
+    @Override
+    public double process(int num) {
+        //To change body of generated methods, choose Tools | Templates.
+        if(num<=threshold)
+            num=0;
+        if(num>=threshold)
+            num=254;
+        
+        return num+0.0;
+    }
+    
+}
